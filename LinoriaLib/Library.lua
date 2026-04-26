efed03b5b96af545a4ec986581b52ef0c25ea3e1
UwUPawz <pagedcomic@gmail.com>
2026-04-27 03:47:06 +0800

    Update Library.lua

--- LinoriaLib/Library.lua
+++ LinoriaLib/Library.lua
@@ -2026,7 +2026,6 @@ Options[Idx] = KeyPicker
 
 return self
 end
-BaseAddonsFuncs.AddKeybind = BaseAddonsFuncs.AddKeyPicker
 
 function BaseAddonsFuncs:AddColorPicker(Idx, Info)
 	local ParentObj = self
